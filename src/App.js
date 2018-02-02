b403113db7f15064c3e878158aa176204e58e5e0
Bryan Jennings <bryanjenningz@gmail.com>
2018-02-02 15:10:02 -0800

    use global block width

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import React, { Component } from "react";
 import "./App.css";
 
 const randomDimensions = () => ({
-  width: Math.random() * 100 + 20,
-  height: Math.random() * 100 + 20
+  width: Math.random() * 16 + 4,
+  height: Math.random() * 16 + 4
 });
+
+const BLOCK_WIDTH = 10;
 
 class App extends Component {
   state = {
     rooms: [randomDimensions()]
-  }
+  };
   render() {
     const { rooms } = this.state;
     return (
       <div>
         <div>Dungeon game</div>
         {rooms.map(({ width, height }, i) => (
-          <div style={{ width, height, background: "white" }} />
+          <div
+            key={i}
+            style={{
+              width: width * BLOCK_WIDTH,
+              height: height * BLOCK_WIDTH,
+              background: "white"
+            }}
+          />
         ))}
       </div>
-    )
+    );
   }
 }
 
