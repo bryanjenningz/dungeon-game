e852085ac95db027e2ff8d9c879c10860e28644c
Bryan Jennings <bryanjenningz@gmail.com>
2018-02-03 14:11:07 -0800

    generate a series of room that are connected

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ const randomDimensions = () => ({
   height: Math.random() * 16 + 4
 });
 
-const randomChoice = range => Math.floor(Math.random() * range);
+const randomInt = range => Math.floor(Math.random() * range);
+const randomChoice = choices => choices[randomInt(choices.length)];
 
 // This function is used to generate a random connection between 2 rooms.
 // This function can be used for connecting 2 rooms vertically or horizontally.
@@ -29,35 +30,85 @@ const randomChoice = range => Math.floor(Math.random() * range);
 const randomRoomConnection = (width1, width2) => {
   const offsetRange = width1 + width2 * 2 - 2;
   const offsetStart = -width2 + 1;
-  const roomOffset = offsetStart + randomChoice(offsetRange);
+  const roomOffset = offsetStart + randomInt(offsetRange);
   const overlapOffsetStart = Math.max(0, roomOffset);
   const overlapOffsetEnd = Math.min(width1, roomOffset + width2);
   const overlapRange = overlapOffsetEnd - overlapOffsetStart + 1;
-  const connectionOffset = overlapOffsetStart + randomChoice(overlapRange);
+  const connectionOffset = overlapOffsetStart + randomInt(overlapRange);
   return { roomOffset, connectionOffset };
+};
+
+const randomRoomWithEmptyConnection = rooms => {
+  let room;
+  let tries = 0;
+  while (
+    !room ||
+    (room.left && room.right && room.top && room.bottom && tries < 10)
+  ) {
+    room = randomChoice(rooms);
+    tries += 1;
+  }
+  return room;
+};
+
+const randomRooms = () => {
+  const ROOM_COUNT = 20;
+  const rooms = [randomDimensions()];
+  const oppositeSide = {
+    left: "right",
+    right: "left",
+    top: "bottom",
+    bottom: "top"
+  };
+  for (let i = 0; i < ROOM_COUNT; i++) {
+    const room = randomRoomWithEmptyConnection(rooms);
+    const side = randomChoice(
+      Object.keys(oppositeSide).filter(side => !room[side])
+    );
+    const newRoom = randomDimensions();
+    const { roomOffset, connectionOffset } = {
+      left: () => randomRoomConnection(newRoom.height, room.height),
+      right: () => randomRoomConnection(room.height, newRoom.height),
+      top: () => randomRoomConnection(newRoom.width, room.width),
+      bottom: () => randomRoomConnection(room.width, newRoom.width)
+    }[side]();
+
+    room[side] = { roomOffset, connectionOffset };
+    newRoom[oppositeSide[side]] = {
+      roomOffset: -roomOffset,
+      connectionOffset: -roomOffset + connectionOffset
+    };
+    rooms.push(newRoom);
+  }
+  return rooms;
 };
 
 const BLOCK_WIDTH = 10;
 
 class App extends Component {
   state = {
-    rooms: [randomDimensions()]
+    rooms: randomRooms()
   };
   render() {
     const { rooms } = this.state;
     return (
       <div>
         <div>Dungeon game</div>
-        {rooms.map(({ width, height }, i) => (
-          <div
-            key={i}
-            style={{
-              width: width * BLOCK_WIDTH,
-              height: height * BLOCK_WIDTH,
-              background: "white"
-            }}
-          />
-        ))}
+        <div style={{ position: "relative" }}>
+          {rooms.map(({ width, height }, i) => (
+            <div
+              key={i}
+              style={{
+                position: "absolute",
+                left: 0,
+                top: i * 20 * BLOCK_WIDTH,
+                width: width * BLOCK_WIDTH,
+                height: height * BLOCK_WIDTH,
+                background: "white"
+              }}
+            />
+          ))}
+        </div>
       </div>
     );
   }
