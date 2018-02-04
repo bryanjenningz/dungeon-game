210bd503f72fc5d091e19b5b4604badd146f93da
Bryan Jennings <bryanjenningz@gmail.com>
2018-02-04 14:29:57 -0800

    make random box generation better

--- src/App.js
+++ src/App.js
@@ -1,109 +1,71 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const randomInt = (lo, hi) => lo + Math.floor(Math.random() * (hi - lo));
+
+const randomChoice = choices => choices[randomInt(0, choices.length)];
+
 const randomDimensions = () => ({
-  id: Math.random().toString,
-  width: Math.random() * 16 + 4,
-  height: Math.random() * 16 + 4
+  width: randomInt(5, 21),
+  height: randomInt(5, 21)
 });
 
-const randomInt = range => Math.floor(Math.random() * range);
-const randomChoice = choices => choices[randomInt(choices.length)];
+const randomSide = () => randomChoice(["left", "right", "top", "bottom"]);
 
-// This function is used to generate a random connection between 2 rooms.
-// This function can be used for connecting 2 rooms vertically or horizontally.
-// A connection between 2 rooms consists of a room offset from the first room
-// and a connection offset with respect to the first room.
-// A negative room offset for a horizontal room connection means the right
-// room is higher than the left room. If the offset is positive, the right room
-// is lower.
-// A negative room offset for a vertical connection means the bottom room is
-// farther to the left. If the room offset is positive, the bottom room is
-// more to the right than the top room.
-// A connection offset can never be negative because it has to be within the
-// overlap of the 2 rooms.
-// If you're trying to connect 2 rooms horizontally, make sure the first
-// argument is the height of the room on the left and the second argument
-// is the height of the room on the right.
-// If you're trying to connect 2 rooms vertically, make sure the first
-// argument is the width of the room on top and the second argument is the
-// width of the room on bottom.
-const randomRoomConnection = (width1, width2) => {
-  const offsetRange = width1 + width2 * 2 - 2;
-  const offsetStart = -width2 + 1;
-  const roomOffset = offsetStart + randomInt(offsetRange);
-  const overlapOffsetStart = Math.max(0, roomOffset);
-  const overlapOffsetEnd = Math.min(width1, roomOffset + width2);
-  const overlapRange = overlapOffsetEnd - overlapOffsetStart + 1;
-  const connectionOffset = overlapOffsetStart + randomInt(overlapRange);
-  return { roomOffset, connectionOffset };
-};
-
-const randomRoomWithEmptyConnection = rooms => {
-  let room;
-  let tries = 0;
-  while (
-    !room ||
-    (room.left && room.right && room.top && room.bottom && tries < 10)
-  ) {
-    room = randomChoice(rooms);
-    tries += 1;
-  }
-  return room;
-};
-
-const randomRooms = () => {
-  const ROOM_COUNT = 20;
-  const rooms = [randomDimensions()];
-  const oppositeSide = {
-    left: "right",
-    right: "left",
-    top: "bottom",
-    bottom: "top"
-  };
-  for (let i = 0; i < ROOM_COUNT; i++) {
-    const room = randomRoomWithEmptyConnection(rooms);
-    const side = randomChoice(
-      Object.keys(oppositeSide).filter(side => !room[side])
-    );
-    const newRoom = randomDimensions();
-    const { roomOffset, connectionOffset } = {
-      left: () => randomRoomConnection(newRoom.height, room.height),
-      right: () => randomRoomConnection(room.height, newRoom.height),
-      top: () => randomRoomConnection(newRoom.width, room.width),
-      bottom: () => randomRoomConnection(room.width, newRoom.width)
+const randomBoxes = count => {
+  const boxes = [{ ...randomDimensions(), x: 0, y: 0 }];
+  for (let i = 1; i < count; i++) {
+    const box = randomChoice(boxes);
+    const side = randomSide();
+    const { width, height } = randomDimensions();
+    const { x, y } = {
+      left: () => ({
+        x: box.x - width,
+        y: box.y + randomInt(-height + 1, box.height - 1)
+      }),
+      right: () => ({
+        x: box.x + box.width,
+        y: box.y + randomInt(-height + 1, box.height - 1)
+      }),
+      top: () => ({
+        x: box.x + randomInt(-width + 1, box.width - 1),
+        y: box.y - height
+      }),
+      bottom: () => ({
+        x: box.x + randomInt(-width + 1, box.width - 1),
+        y: box.y + box.height
+      })
     }[side]();
-
-    room[side] = { roomId: newRoom.id, roomOffset, connectionOffset };
-    newRoom[oppositeSide[side]] = {
-      roomId: room.id,
-      roomOffset: -roomOffset,
-      connectionOffset: -roomOffset + connectionOffset
-    };
-    rooms.push(newRoom);
+    boxes.push({ width, height, x, y });
   }
-  return rooms;
+  let [minX, minY] = [0, 0];
+  for (const box of boxes) {
+    [minX, minY] = [Math.min(minX, box.x), Math.min(minY, box.y)];
+  }
+  for (const box of boxes) {
+    box.x = box.x - minX;
+    box.y = box.y - minY;
+  }
+  return boxes;
 };
 
 const BLOCK_WIDTH = 10;
 
 class App extends Component {
-  state = {
-    rooms: randomRooms()
-  };
+  state = { boxes: randomBoxes(40) };
+
   render() {
-    const { rooms } = this.state;
+    const { boxes } = this.state;
     return (
       <div>
-        <div>Dungeon game</div>
         <div style={{ position: "relative" }}>
-          {rooms.map(({ width, height }, i) => (
+          {boxes.map(({ width, height, x, y }, i) => (
             <div
               key={i}
               style={{
                 position: "absolute",
-                left: 0,
-                top: i * 20 * BLOCK_WIDTH,
+                left: x * BLOCK_WIDTH,
+                top: y * BLOCK_WIDTH,
                 width: width * BLOCK_WIDTH,
                 height: height * BLOCK_WIDTH,
                 background: "white"
